e97aaab98d3a1aca5ae82553a9a9ada164d20301
Kevin Trinh <66029570+KTrinhGit@users.noreply.github.com>
2025-08-03 21:42:32 -0400

    implement asteroid collision

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
 import pygame 
+import sys
 from constants import *
 from player import * 
+from asteroid import * 
+from asteroidfield import * 
 
 def main(): 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group() 
+    asteroids = pygame.sprite.Group() 
+
+    Player.containers = (updatable, drawable)
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
 
     while True: 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+
+        for obj in asteroids: 
+            if obj.collide(player):
+                print("Game Over!")
+                sys.exit()
+        
+        for item in drawable: 
+            item.draw(screen)
         pygame.display.flip()
         delta = clock.tick(60)
         dt = delta / 1000
